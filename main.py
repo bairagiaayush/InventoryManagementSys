f0bae4d3dc72698a000b13ee047732caefb908b7
Aayush Bairagi <76145275+bairagiaayush@users.noreply.github.com>
2023-08-09 22:37:59 +0530

    Update IMS.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import json, sys
+
 records ={}
 with open("records.json") as data_file:
   records = json.load(data_file)
 print(records)
+
+print("\n")
 print("NOTE------>>>")
-print("product keys range from: 1000 - ", int(max(records.keys())))
+print("product UID's ranges from: 1000 - ", int(max(records.keys())))
+print("\n")
 
 # FOLLOWING CODE IS TO ADD A NEW PRODUCT TO OUR INVENTORY
 maximum = -1
-new_product = input("enter 1 if new product to be added to inventory, else enter any other character")
+new_product = input("Enter 1 if new product to be added to inventory, else enter any other character : ")
+
 if new_product == "1":
     try:
         for i in records.keys():
@@ -16,56 +21,61 @@ if new_product == "1":
             maximum = max(maximum,i)
         
         UID = str(maximum + 1)
-        print("UID for this product is", UID)
-        name = input("product name")
-        value = int(input("unit price of product"))
-        inventory = int(input("enter quantity of the product available in storage"))
-        size = int(input("enter size of product"))
-        make = input("enter make of product in CAPITALS")
-        warranty = input("any warranty? enter Yes or No only")
+        print("\n")
+        print("UID for this Product is : ", UID)
+        name = input("Product Name : ")
+        value = int(input("Unit price of product : "))
+        inventory = int(input("Enter quantity of the product available in storage : "))
+        size = int(input("Enter size of product : "))
+        make = input("Enter make of product in CAPITALS : ")
+        warranty = input("Any Warranty? enter Yes or No only : ")
         if warranty == "Yes" or "No":
-            records[UID] = { "product" : name , "value" : value , "inventory" : inventory , "size" : size , "make" : make , "warranty" : warranty}
+            records[UID] = {"product" : name , "value" : value , "inventory" : inventory , "size" : size , "make" : make , "warranty" : warranty}
         else:
             print("INVALID INPUTS")
             sys.exit()
     except:
         print("INVALID INPUTS")
         sys.exit()  
-        
-js = json.dumps(records)
 
+# FOLLOWING CODE IS TO SERIALIZING RECORDS (DICTIONARY) INTO A JSON FORMATTED STRING AND UPDATING THE UPDATED RECORD TO records.json 
+js = json.dumps(records)
 fd = open("records.json",'w')
 fd.write(js)
 fd.close()
 
 # FOLLOWING CODE IS TO CHECK THE INVENTORY FOR A PARTICULAR PRODUCT
-x = input("enter 1 if you want to check stock of a product. Else enter any other character")
+print("\n")
+x = input("Enter 1 if you want to check stock of a product. Else enter any other character : ")
+
 if x =="1":
-    stock = input("enter UID of product to check its inventory: ")
+    stock = input("Enter UID of product to check its inventory: ")
     if stock not in records.keys():
-        print("product not existing. Please enter a vaild ID or add a new product")
+        print("Product not existing. Please enter a vaild ID or add a new product.")
         sys.exit()
     stock_value = records[stock]["inventory"]
     print("Existing stock : ", stock_value)
 
 # FOLLOWING CODE IS TO ADD STOCK TO OUR EXISTING INVENTORY
-x = input("enter 1 if you want to add stock to inventory. Else enter any other character")
+print("\n")
+x = input("Enter 1 if you want to add stock to inventory. Else enter any other character : ")
+
 if x =="1":
-    prod = input("enter UID of product whose stock has to be refilled")
+    prod = input("Enter UID of product whose stock has to be refilled : ")
     if prod not in records.keys():
-        print("product not existing. Please enter a vaild ID or add a new product")
+        print("product not existing. Please enter a vaild ID or add a new product : ")
         sys.exit()
     
-    add = input("enter the number of stock to be added to inventory")
+    add = input("Enter the number of stock to be added to inventory : ")
     try:
         add = int(add)
     except:
-        print("enter numerical value")
+        print("INVALID INPUT")
         sys.exit()
+
     records[prod]["inventory"] = records[prod]["inventory"] + add
     
     js = json.dumps(records)
-    
     fd = open("records.json",'w')
     fd.write(js)
     fd.close()
@@ -77,14 +87,16 @@ for i in records.keys():
     if records[i]["warranty"] == "Yes":
         warranty_products.append(records[i]["product"])
 
+
 # USER GIVING HOW MANY PRODUCTS ARE TO BE BOUGHT BY CUSTOMER
-x = input("enter 1 if a sale is being made. Else enter any other character")
+print("\n")
+x = input("Enter 1 if a sale is being made. Else enter any other character : ")
 if x =="1":
-    number = input("enter total number of products to be bought")
+    number = input("Enter total number of products to be bought : ")
     try:
         number = int(number)
     except:
-        print("enter valid number")
+        print("INVALID INPUT")
         sys.exit()
     if number > len(records.keys()) or number < 1:
         print("cant process these many products")
@@ -94,45 +106,46 @@ if x =="1":
     total_payment = 0
     sale = {}
     for i in range(number):
-        product = input("enter product ID")
+        product = input("Enter product ID : ")
         if product not in records.keys():
-            print("enter valid product ID")
+            print("INVALID product ID")
             sys.exit()
           
-        print("the accessed product is: ", records[product])
+        print("The accessed product is: ", records[product])
         
-        quantity = input("enter quantity to be purchased")
+        quantity = input("Enter quantity to be purchased : ")
         try:
             quantity = int(quantity)
         except:
-            print("enter valid quantity")
+            print("INVALID INPUT")
             sys.exit()
         if int(quantity) < 1:
-            print("enter valid quantity")
+            print("INVALID INPUT")
             sys.exit()
-          
         if quantity > records[product]["inventory"]:
             print("not enough inventory")
             sys.exit()
         
         records[product]["inventory"] = records[product]["inventory"] - quantity
         unit_price = records[product]["value"]
-        total = records[product]["value"]*quantity
-        total_payment = total_payment + records[product]["value"]*quantity
+        # total = records[product]["value"]*quantity
+        total = unit_price*quantity
+        # total_payment = total_payment + records[product]["value"]*quantity
+        total_payment = total_payment + total
         
         sale[records[product]["product"]] = [unit_price , quantity , total]
     
     
     # PRINTING THE SALE RECIEPT FOR THE CUSTOMER
     from datetime import date
-    from datetime import datetime
+    import time
     today = date.today()
     d = today.strftime("%d/%m/%Y")
-    now = datetime.now()
-    curr_time = now.strftime("%H:%M:%S")
-    print("sale reciept is as follows")
+    curr_time = time.strftime('%H:%M:%S')
+    print("\n")
+    print("Sale reciept is as follows")
     print("--------------------------")
-    
+
     for i in sale.keys():
         print("product: ", i ,"||  unit price: ", sale[i][0], "||   quantity ", sale[i][1] ,"||   total ", sale[i][2])
         if i in warranty_products:
@@ -164,6 +177,7 @@ if x =="1":
     with open(filename,'r') as file:
         data = json.load(file)
         
+        print(data)
     data.append(entry)
     
     with open(filename,'w') as file:
